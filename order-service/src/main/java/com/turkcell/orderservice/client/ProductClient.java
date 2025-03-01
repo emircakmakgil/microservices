40d49253af580fa6d49bd83cc831023f98f44e2c
emircakmakgil <emircakmakgil@marun.edu.tr>
2025-03-01 17:44:10 +0300

    add docker-compose.yml

---
+++ order-service/src/main/java/com/turkcell/orderservice/client/ProductClient.java
@@ -0,0 +1,10 @@
+package com.turkcell.orderservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name="product-service")
+public interface ProductClient {
+    @GetMapping("/api/v1/products")
+    String get();
+}
--- order-service/src/main/java/com/turkcell/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/turkcell/orderservice/controller/OrderController.java
@@ -1,14 +1,26 @@
 package com.turkcell.orderservice.controller;
 
+import com.turkcell.orderservice.client.ProductClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
+    private final ProductClient productClient;
+
+    public OrderController(ProductClient productClient) {
+        this.productClient = productClient;
+    }
+
     @GetMapping
     public String get(){
+        String response = productClient.get();
+        System.out.println(response);
+
         return "ORDER SERVICE";
     }
 }
